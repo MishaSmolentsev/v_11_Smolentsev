6aa1580584dd6d6502df19a8d9bf1c6fa00916b7
MishaSmolentsev <zakaz54@mail.ru>
2020-12-30 14:02:39 +0700

    ekzamen

---
+++ app/src/main/java/com/example/smolentsev_v_11/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.smolentsev_v_11;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends AppCompatActivity {
+    ViewFlipper flipper;
+Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addListenerOnButton();
+    }
+    public void addListenerOnButton () {
+        btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(
+                new View.OnClickListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(".second_activity");
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+}
---
+++ app/src/main/java/com/example/smolentsev_v_11/second_activity.java
@@ -0,0 +1,13 @@
+package com.example.smolentsev_v_11;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class second_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
